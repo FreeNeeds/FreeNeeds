54661a026e93c48c1c6875e6f174a19605da34cc
sungmunjo <80288379+sungmunjo@users.noreply.github.com>
2022-09-13 17:40:52 +0900

    feature : 회원가입 페이지 구현(진행중)

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -19,11 +19,25 @@
           >비밀번호</label
         >
         <div class="regist-input-decoration">
-          <input
-            type="password"
-            id="registerCPasswordInput"
-            class="regist-input-wrapper password-input-wrapper"
-          />
+          <div class="row">
+            <div class="col-10">
+              <input
+                type="password"
+                id="registerCPasswordInput"
+                class="regist-input-wrapper  password-input-wrapper"
+              />
+            </div>
+            <div
+              class="col-2"
+              v-if="passwordvisible"
+              @click="changePasswordVisible"
+            >
+              <img src="@/assets/images/eyeopen.png" class="eye-wrapper" />
+            </div>
+            <div class="col-2" v-else @click="changePasswordVisible">
+              <img src="@/assets/images/eyeclose.png" class="eye-wrapper" />
+            </div>
+          </div>
         </div>
       </div>
       <div class="register-item-wrapper">
@@ -31,11 +45,30 @@
           >비밀번호 확인</label
         >
         <div class="regist-input-decoration">
-          <input
-            type="password"
-            id="registerCPasswordConfirmInput"
-            class="regist-input-wrapper  password-input-wrapper"
-          />
+          <div class="row">
+            <div class="col-10">
+              <input
+                type="password"
+                id="registerCPasswordConfirmInput"
+                class="regist-input-wrapper  password-input-wrapper"
+              />
+            </div>
+            <div
+              class="col-2"
+              v-if="passwordConfirmvisible"
+              @click="changePasswordConfirmVisible"
+            >
+              <img src="@/assets/images/eyeopen.png" class="eye-wrapper" />
+            </div>
+            <div class="col-2" v-else @click="changePasswordConfirmVisible">
+              <img src="@/assets/images/eyeclose.png" class="eye-wrapper" />
+            </div>
+          </div>
+
+          <!-- <div v-if="passwordConfirmvisible"></div>
+          <div v-else>
+            <img ref="@/assets/images/eyeclose.png" />
+          </div> -->
         </div>
       </div>
       <div class="register-item-wrapper">
@@ -62,6 +95,43 @@
           />
         </div>
       </div>
+      <div class="register-item-wrapper">
+        <label for="registerCNumberInput" class="register-input-label"
+          >이메일</label
+        >
+        <div class="regist-input-decoration">
+          <div class="row">
+            <div class="col-6">
+              <input
+                type="text"
+                id="registerCNumberInput"
+                class="regist-email-input-wrapper"
+              />
+            </div>
+            <div
+              class="col-1"
+              style="text-align:center;font-size:20px; padding-top: 10px;"
+            >
+              @
+            </div>
+            <div class="col-5">
+              <select
+                name="emailadress"
+                id="emailSelectBar"
+                class="form-select"
+                aria-label="Default select example"
+              >
+                <option value="gmail.com" selected>gmail.com</option>
+                <option value="naver.com">naver.com</option>
+                <option value="daum.net">daum.net</option>
+                <option value="hanmail.net">hanmail.net</option>
+                <option value="kakao.com">kakao.com</option>
+              </select>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div></div>
     </div>
   </div>
 </template>
@@ -77,10 +147,32 @@ export default {
         name: "",
         password: "",
         passwordConfirm: ""
-      }
+      },
+      passwordvisible: false,
+      passwordConfirmvisible: false
     };
   },
   methods: {
+    changePasswordConfirmVisible() {
+      this.passwordConfirmvisible = !this.passwordConfirmvisible;
+      var passwordElement = document.getElementById(
+        "registerCPasswordConfirmInput"
+      );
+      if (passwordElement.type == "password") {
+        passwordElement.type = "text";
+      } else {
+        passwordElement.type = "password";
+      }
+    },
+    changePasswordVisible() {
+      this.passwordvisible = !this.passwordvisible;
+      var passwordElement = document.getElementById("registerCPasswordInput");
+      if (passwordElement.type == "password") {
+        passwordElement.type = "text";
+      } else {
+        passwordElement.type = "password";
+      }
+    },
     register() {
       var vm = this;
 
@@ -106,6 +198,19 @@ export default {
 </script>
 
 <style>
+.regist-email-input-wrapper {
+  padding: 10px;
+  font-size: 20px;
+  width: 40%;
+  border: none;
+  outline: none;
+}
+.eye-wrapper {
+  margin-left: 25%;
+  margin-top: 25%;
+  height: 50%;
+  weight: 50%;
+}
 .register-wrapper {
   margin-top: 10%;
   width: 50%;
@@ -130,12 +235,16 @@ export default {
   display: block;
 }
 .password-input-wrapper {
-  width: 80%;
+  width: 90%;
 }
 .regist-input-decoration {
   border: 1px solid #c7c8d2;
   border-radius: 8px;
   background-color: white;
   padding: 3px;
+}
+#emailSelectBar {
+  width: 100%;
+  height: 100%;
 }
 </style>
