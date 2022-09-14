9fa56fd988343f3e31e5d3278d46cb73a7f254d3
chodayeon <dus6982@naver.com>
2022-09-14 13:53:17 +0900

    Fix : 폴더 경로 이동

---
+++ backend/src/main/java/com/ssafy/backend/api/service/UserService.java
@@ -0,0 +1,17 @@
+package com.ssafy.backend.api.service;
+
+import com.ssafy.backend.api.request.UserRegisterPostReq;
+import com.ssafy.backend.db.entity.User;
+
+import java.util.Optional;
+
+/**
+ *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
+ */
+public interface UserService {
+	User createUser(UserRegisterPostReq userRegisterInfo);
+	Optional<User> getUserByUsername(String username);
+	void validateDuplicateMember(UserRegisterPostReq userRegisterInfo);
+
+	public Optional<User> getUserByUserId(Long userId);
+}
