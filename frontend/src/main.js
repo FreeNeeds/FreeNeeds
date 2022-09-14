6f6a8a05a766d3f5997fe8221f311e26476ed354
sungmunjo <80288379+sungmunjo@users.noreply.github.com>
2022-09-14 10:38:30 +0900

    feature : 기업회원가입

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 Vue.config.productionTip = false;
 Vue.filter("truncate", function(text, length, clamp) {
   if (text) {
@@ -16,6 +18,6 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount("#app");
-
+Vue.use(BootstrapVue);
 //Vue.component("HNav", HNav);
 //Vue.component("HBreadcrumb", HBreadcumb);
