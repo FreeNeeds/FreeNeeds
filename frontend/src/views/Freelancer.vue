6267b73e8f794f42c09478be218cdf7dfc0ee569
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-09-15 17:27:16 +0900

    feature : freelancer add freelancercard

--- frontend/src/views/Freelancer.vue
+++ frontend/src/views/Freelancer.vue
@@ -1,24 +1,22 @@
 <template>
   <b-container>
     <header-nav></header-nav>
-    <FilterBtn></FilterBtn>
+    <button id="filterBtn">
+      필터
+    </button>
     <FreelancerList></FreelancerList>
     <FooterNav></FooterNav>
   </b-container>
-
 </template>
 
 <script>
-import HeaderNav from '@/components/HeaderNav.vue';
-import FooterNav from '@/components/FooterNav.vue';
-import FilterBtn from '@/components/Freelancer/FilterBtn.vue';
-import FreelancerList from '@/components/Freelancer/FreelancerList.vue';
+import HeaderNav from "@/components/HeaderNav.vue";
+import FooterNav from "@/components/FooterNav.vue";
+import FreelancerList from "@/components/Freelancer/FreelancerList.vue";
 
 export default {
-  components: {HeaderNav, FooterNav, FilterBtn, FreelancerList },
-}
+  components: { HeaderNav, FooterNav, FreelancerList }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
