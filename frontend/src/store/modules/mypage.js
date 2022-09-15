0e08eb0a290a87d7c6a80b3a9cf9e1097f5e9074
sungmunjo <80288379+sungmunjo@users.noreply.github.com>
2022-09-15 17:32:27 +0900

    feature : 프리랜서 나의 지원현황 페이지 구조

--- frontend/src/store/modules/mypage.js
+++ frontend/src/store/modules/mypage.js
@@ -2,18 +2,21 @@ import axios from "axios";
 import instance from "@/api/index.js";
 import router from "@/router/index.js";
 
-import companymenus from "@/views/menus/mypageCompanyMenu.json";
-import freelancermenus from "@/views/menus/mypageFreelancerMenu.json";
-
+/** 메뉴 json파일 */
+import companymenus from "@/views/menus/mypage/mypageCompanyMenu.json";
+import freelancermenus from "@/views/menus/mypage/mypageFreelancerMenu.json";
+import applystatusmenus from "@/views/menus/applystatus/applystatusmenus.json";
 export default {
   state: () => ({
     mypageCompanyMenu: companymenus,
-    mypageFreelancerMenu: freelancermenus
+    mypageFreelancerMenu: freelancermenus,
+    applyStatusMenus: applystatusmenus
   }),
   mutations: {},
   getters: {
     mypageCompanyMenu: state => state.mypageCompanyMenu,
-    mypageFreelancerMenu: state => state.mypageFreelancerMenu
+    mypageFreelancerMenu: state => state.mypageFreelancerMenu,
+    applyStatusMenus: state => state.applyStatusMenus
   },
   actions: {}
 };
