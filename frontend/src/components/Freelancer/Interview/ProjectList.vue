fa31fbf83dd949e6a3462d45de5c7a331612d45e
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-09-16 16:09:02 +0900

    feature : create Interview.vue

---
+++ frontend/src/components/Freelancer/Interview/ProjectList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card-group">
+    <ProjectCard></ProjectCard>
+  </div>
+</template>
+
+<script>
+import ProjectCard from "@/components/ProjectCard/ProjectCard.vue";
+export default {
+  components: {
+    ProjectCard
+  }
+};
+</script>
+
+<style></style>
