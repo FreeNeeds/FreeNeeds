59b058818c7af81d1d71e98408b7622035f7e667
susan1231 <susan1231@naver.com>
2022-09-18 17:37:37 +0900

    Fix: ResponseBody  수정

--- backend/src/main/java/com/ssafy/backend/api/controller/CompanyController.java
+++ backend/src/main/java/com/ssafy/backend/api/controller/CompanyController.java
@@ -4,7 +4,6 @@ import com.ssafy.backend.api.request.CompanyInfoPostReq;
 import com.ssafy.backend.api.request.CompanyRegisterPostReq;
 import com.ssafy.backend.api.response.CompanyInfoRes;
 import com.ssafy.backend.api.response.CompanyRes;
-import com.ssafy.backend.api.response.CompanyUpdateRes;
 import com.ssafy.backend.api.service.CompanyService;
 import com.ssafy.backend.common.auth.SsafyCompanyDetails;
 import com.ssafy.backend.common.model.response.BaseResponseBody;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import java.util.Map;
-import java.util.Optional;
 
 @Api(value = "기업 API", tags = {"Company"})
 @RestController
@@ -41,8 +39,7 @@ public class CompanyController {
             @RequestBody @ApiParam(value="회원가입 정보", required = true)@Validated CompanyRegisterPostReq registerInfo) {
 
         //임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-        Company company = companyService.createCompany(registerInfo);
-
+        companyService.createCompany(registerInfo);
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
     }
 
@@ -69,8 +66,8 @@ public class CompanyController {
     public ResponseEntity<? extends BaseResponseBody> updateCompany(@ApiIgnore Authentication authentication, @RequestBody Map<Object, Object> objectMap) {
         SsafyCompanyDetails companyDetails = (SsafyCompanyDetails)authentication.getDetails();
         String username = companyDetails.getUsername();
-        Company company = companyService.updateCompany(username, objectMap);
-        return ResponseEntity.status(200).body(CompanyUpdateRes.of(200, "Success", company));
+        companyService.updateCompany(username, objectMap);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
     }
 
 
@@ -83,8 +80,8 @@ public class CompanyController {
         SsafyCompanyDetails companyDetails = (SsafyCompanyDetails)authentication.getDetails();
         String username = companyDetails.getUsername();
         Company company = companyService.getCompanyByUsername(username).get();
-        CompanyInfo companyInfo = companyService.createCompanyInfo(company, companyInfoPostReq);
-        return ResponseEntity.status(200).body(CompanyInfoRes.of(200, "Success", companyInfo));
+        companyService.createCompanyInfo(company, companyInfoPostReq);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
     }
 
     @GetMapping("/information/{username}")
@@ -94,7 +91,7 @@ public class CompanyController {
     })
     public ResponseEntity<? extends BaseResponseBody> getCompanyInfo(@PathVariable String username) {
         CompanyInfo companyInfo = companyService.getCompanyInfoByCompanyUsername(username).get();
-        return ResponseEntity.status(200).body(CompanyInfoRes.of(200, "기업정보 조회 성공", companyInfo));
+        return ResponseEntity.status(200).body(CompanyInfoRes.of(200, "Success", companyInfo));
     }
 
     @PatchMapping("/information")
@@ -105,7 +102,7 @@ public class CompanyController {
     public ResponseEntity<? extends BaseResponseBody> updateCompanyInfo(@ApiIgnore Authentication authentication, @RequestBody Map<Object, Object> objectMap) {
         SsafyCompanyDetails companyDetails = (SsafyCompanyDetails)authentication.getDetails();
         String username = companyDetails.getUsername();
-        CompanyInfo companyInfo = companyService.updateCompanyInfo(username, objectMap);
-        return ResponseEntity.status(200).body(CompanyInfoRes.of(200, "기업정보 수정 성공", companyInfo));
+        companyService.updateCompanyInfo(username, objectMap);
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
     }
 }
