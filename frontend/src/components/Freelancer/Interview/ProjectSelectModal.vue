077040daf57c45a8f438fcafaa649d46443d3f43
JaeYeongPark <fly_98@naver.com>
2022-09-19 12:55:10 +0900

    feat : 프리랜서 인터뷰 선택시 모달창 보완

--- frontend/src/components/Freelancer/Interview/ProjectSelectModal.vue
+++ frontend/src/components/Freelancer/Interview/ProjectSelectModal.vue
@@ -11,14 +11,14 @@
     <div class="modal-dialog" id="ProjectSelectModalWrapper">
       <div class="modal-content" id="ProjectSelectModalContent">
         <h3>인터뷰 신청이 완료되었습니다.</h3>
-        <div id="CloseBtnWrapper">
-          <button
+          <div id="modal-close-btn-wrapper">
+            <button
             type="button"
-            id="ProjectModalModalCloseBtn"
-            class="btn-close"
+            class="project-select btn btn-primary rounded-pill"
+            id="modal-close-btn"
             data-bs-dismiss="modal"
             aria-label="Close"
-          >
+            >
             확인
           </button>
         </div>
@@ -34,20 +34,26 @@ export default {};
 <style>
 #ProjectSelectModal {
   background-color: rgba(0, 0, 0, 0.2) !important;
+  padding: 150px;
 }
 #ProjectSelectModalWrapper {
   margin-top: 50px;
   text-align: center;
 }
 #ProjectSelectModalContent {
-  width: 600px;
+  width: 500px;
   margin: 0 auto;
   border-radius: 15px;
-  height: 800px;
+  height: 250px;
   overflow-y: auto;
+  padding: 50px;
 }
-#CloseBtnWrapper {
-  text-align: right;
-  padding: 30px;
+#modal-close-btn-wrapper{
+  height:100px;
+  padding:50px;
+}
+#modal-close-btn{
+  margin:0 auto;
+  margin-top:20px;
 }
 </style>
