ca89d9271e482c190d6ee6902641ec016ac5a4ac
유영진 <dugajada032@gmail.com>
2022-09-22 10:14:04 +0900

    Feat : 충돌 해결

--- backend/src/main/java/com/ssafy/backend/db/repository/DomainRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.backend.db.repository;
-
-import com.ssafy.backend.db.entity.Domain;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DomainRepository extends JpaRepository<Domain, Long> {
-}
