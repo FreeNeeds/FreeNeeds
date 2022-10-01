fabd82c15fdf6d42a9fed75e42df295605f09945
JaeyeongPark <99472062+JaeyeongPark@users.noreply.github.com>
2022-10-01 14:47:12 +0900

    test

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 //import { API_BASE_URL } from "../config";
 
 
-const API_BASE_URL = "http://localhost:8080/api/v1a";
+const API_BASE_URL = "http://localhost:8080/api/v1aaaaaa";
 
 
 function createInstance() {
