c758ad6ed8de143b82358d51edc2791d91996db7
유영진 <dugajada032@gmail.com>
2022-10-02 18:14:38 +0900

    feat: 컨트랙트 patch

--- backend/src/main/java/com/ssafy/backend/api/service/ContractService.java
+++ backend/src/main/java/com/ssafy/backend/api/service/ContractService.java
@@ -4,9 +4,15 @@ import com.ssafy.backend.api.request.ContractRegisterPostReq;
 import com.ssafy.backend.db.entity.Contract;
 import com.ssafy.backend.db.entity.Project;
 import com.ssafy.backend.db.entity.User;
+import io.swagger.annotations.ApiParam;
+
+
 
 public interface ContractService {
     void createContract(Project project, User user, ContractRegisterPostReq registerContractInfo);
 
     Contract getContractByUserIdAndProjectId(Long userId, Long projectId);
-}
+
+    void updateContract( Long userId, Long projectId, String content) ;
+
+    }
